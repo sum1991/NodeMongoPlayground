2a5c13a7ac3668cec4849ca3c84a4cffef173c54
sum1991 <ls.sumukh@gmail.com>
2017-10-10 21:59:23 -0600

    some more practice

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ if(err)
     throw err;
 }
 else{
-    console.log("connected")
+    var collection = db.collection('test_insert');
+    collection.insert({a:2},function(err,docs){
+        collection.count(function(err,count){
+            console.log(format("count=%s",count))
+        })
+    });
+    collection.find().toArray(function(err,results){
+        console.dir(results);
+        db.close();
+    });
 }
-db.close();
+
 })
